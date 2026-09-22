4ca37d393459d23b27053b962bf268e9e89037e2
Jashanjot Singh <145551994+Jashanjotsinghgit@users.noreply.github.com>
2026-09-22 08:28:22 +0530

    webhhoook

--- Test.java
+++ Test.java
@@ -32,7 +32,7 @@ public class Test {
     public static void main(String[] args) {
         int x = 589;
         int y = 10;
-
+        System.out.println("Hi Jashan this is your repo");
         add(x, y);
         sub(x, y);
         mul(x, y);
